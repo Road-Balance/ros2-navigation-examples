8727397edbb6ad2dc82eb68a6e0edefdc785fbbc
kimsooyoung <tge1375@naver.com>
2021-10-25 23:24:57 +0900

    Feature : basic mobile robot descrition

--- basic_mobile_robot/launch/mobile_robot_viz.launch.py
+++ basic_mobile_robot/launch/mobile_robot_viz.launch.py
@@ -7,7 +7,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
