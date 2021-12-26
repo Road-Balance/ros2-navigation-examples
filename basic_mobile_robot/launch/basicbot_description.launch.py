df18e1f1982673c834baea6b4db5a582f289d0e0
kimsooyoung <tge1375@naver.com>
2021-12-27 04:09:20 +0900

    Feature : my version description

---
+++ basic_mobile_robot/launch/basicbot_description.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler, TimerAction
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+# from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+import xacro
+
+def generate_launch_description():
+
+    pkg_path = os.path.join(get_package_share_directory('basic_mobile_robot'))
+
+    # Prepare Robot State Publisher Params
+    urdf_file = os.path.join(pkg_path, 'models', 'basic_mobile_bot_v1.urdf')
+    doc = xacro.parse(open(urdf_file))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    rviz_config_file = os.path.join(pkg_path, 'rviz', 'urdf_config.rviz')
+
+    # Launch RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
+    return LaunchDescription([
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=joint_state_publisher,
+        #         on_exit=[joint_state_publisher_gui],
+        #     )
+        # ),
+        joint_state_publisher,
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        TimerAction(
+            period=3.0, 
+            actions=[rviz]
+        ),
+    ])
